627010beb8e9b594c90a64367e0cced1368f1b98
claudio-dev1993 <claudinhoroberto2008@gmail.com>
2022-04-18 23:07:34 -0300

    Role and User ORM, Servers config, Controllers exeptions

--- src/main/java/com/devsuperior/bds04/entities/User.java
+++ src/main/java/com/devsuperior/bds04/entities/User.java
@@ -1,29 +1,48 @@
 package com.devsuperior.bds04.entities;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-public class User {
-	
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
 	private String password;
-	
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", 
+	joinColumns = @JoinColumn(name = "user_id"), 
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
-	
+
 	public User() {
-		
+
 	}
 
-	
 	public User(Long id, String email, String password, Set<Role> roles) {
 		this.id = id;
 		this.email = email;
 		this.password = password;
 		this.roles = roles;
 	}
-
 
 	public Long getId() {
 		return id;
@@ -48,7 +67,7 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
@@ -69,6 +88,5 @@ public class User {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
